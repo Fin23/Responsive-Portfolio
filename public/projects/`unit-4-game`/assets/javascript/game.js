944fac3796e88fd205696df203299e65d2e586e2
Ross Jameson <Rossjameson1623@gmail.com>
2019-12-06 09:09:02 -0600

    adding styles

--- public/projects/`unit-4-game`/assets/javascript/game.js
+++ public/projects/`unit-4-game`/assets/javascript/game.js
@@ -1,4 +1,4 @@
-var raiden = {
+let raiden = {
   Health: 210,
   Attack: 30,
   enemyHealth: 210,
@@ -7,7 +7,7 @@ var raiden = {
     console.log(this);
   },
 };
-var scorpion = {
+let scorpion = {
   Health: 170,
   Attack: 25,
   enemyHealth: 170,
@@ -16,7 +16,7 @@ var scorpion = {
     console.log(this);
   },
 };
-var sweetTooth = {
+let sweetTooth = {
   Health: 190,
   Attack: 40,
   enemyHealth: 190,
@@ -25,7 +25,7 @@ var sweetTooth = {
     console.log(this);
   },
 };
-var liuKang = {
+let liuKang = {
   Health: 200,
   Attack: 45,
   enemyHealth: 200,
@@ -44,14 +44,14 @@ console.log(this);
 
 //Dom manipulation
 //find hero area to put 1st clicked character in
-var heroArea = document.getElementById('hero-area');
-var enemyArea = document.getElementById('enemy-area');
-var heroStats = document.getElementById('hero-stats');
-var enemyStats = document.getElementById('enemy-stats');
-var heroHealthDisplay = document.getElementById('hero-health');
-var heroAttackDisplay = document.getElementById('hero-attack');
-var enemyHealthDisplay = document.getElementById('enemy-health');
-var enemyAttackDisplay = document.getElementById('enemy-attack');
+let heroArea = document.getElementById('hero-area');
+let enemyArea = document.getElementById('enemy-area');
+let heroStats = document.getElementById('hero-stats');
+let enemyStats = document.getElementById('enemy-stats');
+let heroHealthDisplay = document.getElementById('hero-health');
+let heroAttackDisplay = document.getElementById('hero-attack');
+let enemyHealthDisplay = document.getElementById('enemy-health');
+let enemyAttackDisplay = document.getElementById('enemy-attack');
 //"save" origianal attack value
 
 // function for button click to pick characters and insert into hero area
@@ -147,11 +147,11 @@ var heroAttack = 0;
 var enemyHealth = 0;
 var enemyAttack = 0;
 
-var turnCounter = 0;
-var killCount = 0;
+let turnCounter = 0;
+let killCount = 0;
 
-var increaseAttack = 35;
-var increaseHealth = 100;
+let increaseAttack = 35;
+let increaseHealth = 100;
 
 // fire base
 
@@ -197,7 +197,7 @@ function isEnemyAlive() {
     heroAttack = heroAttack + increaseAttack;
     enemyArea.innerHTML = null;
 
-    //$("#fight").off('click');
+  
 
     alert('you win');
     return true;
@@ -229,33 +229,5 @@ $('#fight').on('click', function() {
   }
   console.log(turnCounter, killCount);
 
-  //firebase funcionality
-  //  $("#fight").on("click", function (event) {
-  //      event.preventDefault();
 
-  //       hHealth = heroHealth.val().trim();
-  //       hAttack = heroAttack.val().trim();
-  //       eHealth = enemyHealth.val().trim();
-  //       eAttack= enemyAttack.val().trim();
-
-  //   database.ref().push({
-  //       hHealth: heroHealth,
-  //       hAttack: heroAttack,
-  //       eHealth: enemyHealth,
-  //       eAttack: enemyAttack,
-  //       dateAdded: firebase.database.ServerValue.TIMESTAMP
-
-  //   });
-  //  });
-
-  //  database.ref().on("child_added", function(snapshot) {
-  //      var sv = snapshot.val();
-
-  //    console.log(sv.hHealth);
-  //      console.log(sv.hAttack);
-  //      console.log(sv.eHealth);
-  //      console.log(sv.eAttack);
-  //      console.log(sv.dateAdded);
-  //      console.log(ts.toDateString());
-  //      });
 });
